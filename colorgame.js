466db8a0c22643b734237da5efc9fcdac0415701
Bwizz245 <Bwizz245@gmail.com>
2019-03-07 11:26:24 -0600

    initial commit

---
+++ colorgame.js
@@ -0,0 +1,57 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square")
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor.toUpperCase();
+});
+
+colorDisplay.textContent = pickedColor.toUpperCase();
+
+for(var i = 0; i < squares.length; i++){
+  squares[i].style.backgroundColor = colors[i];
+
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+    if(clickedColor === pickedColor){
+      for(var j = 0; j < squares.length; j++){
+        squares[j].style.backgroundColor = pickedColor;
+        h1.style.backgroundColor = pickedColor;
+        messageDisplay.textContent = "Correct";
+      }
+    }
+    else
+    {
+      this.style.backgroundColor = "#101010";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+}
+
+function pickColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+
+  for(var i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
